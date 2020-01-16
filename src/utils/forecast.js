0b18615b5d7432660fdf543531965271b7d82bef
Evgeny Bondarev <eugenecooper1988@gmail.com>
2020-01-16 12:51:00 +0200

    Switch to SI system, adding one more line to the output message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,28 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/b6eaa2a93416006aad9113b90e438f86/' + latitude + ',' + longitude
+	const url =
+		'https://api.darksky.net/forecast/b6eaa2a93416006aad9113b90e438f86/' +
+		latitude +
+		',' +
+		longitude +
+		'?units=si'
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
-        }
-    })
+	request({ url, json: true }, (error, { body }) => {
+		if (error) {
+			callback('Unable to connect to weather service!', undefined)
+		} else if (body.error) {
+			callback('Unable to find location', undefined)
+		} else {
+			callback(
+				undefined,
+                `${body.daily.data[0].summary}
+                It is currently ${body.currently.temperature} Celsius degrees out. 
+                Today's high is ${body.daily.data[0].temperatureHigh}.
+                There is a ${body.currently.precipProbability} % chance of rain.`
+			)
+		}
+	})
 }
 
 module.exports = forecast
